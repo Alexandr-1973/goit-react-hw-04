2fd0fea3e2857d0ea094127864b3206098b6266a
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-04-05 13:13:39 +0300

    api1

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,19 @@ import { useEffect, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
-import axios from "axios";
+// import axios from "axios";
+import fetchServer from "./api/fotos-api";
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  // async function axiosGet() {
-  //   const data = await axios.get(
-  //     "https://api.unsplash.com/photos?client_id=ahhcIzD1njr8w5SDdkwapZgiC8vxXk9AN6nr5KfkI3k"
-  //   );
-  //   console.log(data);
-  // }
-//   useEffect(() => {
-//   async function axiosGet() {
-//     // const params = {
-//     //   query: "cat",
-//     // }
-//     const galId = "ahhcIzD1njr8w5SDdkwapZgiC8vxXk9AN6nr5KfkI3k";
-//     const searchText = "office";
-//     const res = await axios.get(
-//       `https://api.unsplash.com/search/photos?client_id=ahhcIzD1njr8w5SDdkwapZgiC8vxXk9AN6nr5KfkI3k&query=cat`
-//     );
-//     console.log(res.data);
-//   }
-
-//   axiosGet();
-// })
-  async function axiosGet() {
-    // const params = {
-    //   query: "cat",
-    // }
-    const galId = "ahhcIzD1njr8w5SDdkwapZgiC8vxXk9AN6nr5KfkI3k";
-    const searchText = "office";
-    const res = await axios.get(
-      `https://api.unsplash.com/search/photos?client_id=${galId}&query=${searchText}`
-    );
-    console.log(res.data);
-  }
-
-  axiosGet();
+  useEffect(() => {
+    async function getInfoServer(searchText, pageNumber, perPage) {
+      const infoData = await fetchServer(searchText, pageNumber, perPage);
+      console.log(infoData);
+    }
+    getInfoServer("cat", 1, 15);
+  });
 
   return <></>;
 }
--- src/api/fotos-api.js
+++ src/api/fotos-api.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 
-async function axiosGet() {
-  const data = await axios.get(
-    "https://api.unsplash.com/search/photos?page=1&query=office"
-  );
-  console.log(data);
+async function fetchServer(searchText, perPage, pageNumber) {
+  const BASE_URL = "https://api.unsplash.com";
+  const SEARCH_HEADER = "/search/photos";
+  const Client_ID = "ahhcIzD1njr8w5SDdkwapZgiC8vxXk9AN6nr5KfkI3k";
+  const url = `${BASE_URL}${SEARCH_HEADER}`;
+  const params = {
+    client_id: Client_ID,
+    query: searchText,
+    // image_type: "photo",
+    // orientation: "horizontal",
+    // safesearch: true,
+    // q: searchText,
+    per_page: perPage,
+    page: pageNumber,
+  };
+  const res = await axios.get(url, { params });
+  return res.data;
 }
 
-axiosGet();
+export default fetchServer;
